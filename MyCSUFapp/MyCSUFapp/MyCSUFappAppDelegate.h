67dc5060976db446423a156fbc5bf8abd21c0058
Bert Aguilar <bert@stackpointer.wlan.csufresno.edu>
2011-09-21 17:26:50 -0700

    added a note

--- MyCSUFapp/MyCSUFapp/MyCSUFappAppDelegate.h
+++ MyCSUFapp/MyCSUFapp/MyCSUFappAppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Bert Aguilar on 9/19/11.
 //  Copyright 2011 Apple Inc. All rights reserved.
 //
-
+// Making an update
 #import <UIKit/UIKit.h>
 
 @interface MyCSUFappAppDelegate : NSObject <UIApplicationDelegate>
